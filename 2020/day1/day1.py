80c2de1f0735edcda9d9bf8d2d2e5aab1e2d6723
Lukas <segelmark.lukas@gmail.com>
2020-12-01 23:42:19 +0100

    Made my solution a little less stupid

--- 2020/day1/day1.py
+++ 2020/day1/day1.py
@@ -1,53 +1,57 @@
+import time
+
+start = time.time()
+
 validation1 = """1721
 979
 366
 299
 675
-1456"""
+1456""".splitlines()
 
 validation2 = """1235
 979
 366
 675
 299
-1721""" 
+1721""".splitlines()
 
 correctAnswer1=514579
 correctAnswer2=241861950
 
-def day1(input):
+def day1(data):
+    data = [int(x) for x in data]
     i=0
     j=0
-    while(i<(len(input)-1)):
+    while(i<(len(data)-1)):
         j=i+1
-        while(j<len(input)):
-            # print(input[i] + " + " + input[j] + " = " + str(int(input[i])+int(input[j])))
-            if(int(input[i])+int(input[j])==2020):
-                print(input[i] + " + " + input[j] + " = " + str(int(input[i])+int(input[j])))
-                return int(input[j])*int(input[i])
+        while(j<len(data)):
+            if(data[i]+data[j]==2020):
+                print(f"{data[i]} + {data[j]} = {data[i]+data[j]}")
+                return data[j]*data[i]
             j=j+1
         i=i+1
 
-def day2(input):
+def day2(data):
+    data = [int(x.strip()) for x in data]
     i=0
     j=0
     k=0
-    while(i<(len(input)-2)):
+    while(i<(len(data)-2)):
         j=i+1
-        while(j<len(input)-1):
+        while(j<len(data)-1):
             k=j+1
-            while(k<len(input)):
-                # print(input[i] + " + " + input[j] + " = " + str(int(input[i])+int(input[j])))
-                if(int(input[i])+int(input[j])+int(input[k])==2020):
-                    print(input[i] + " + " + input[j] + " + " + input[k] + " = " + str(int(input[i])+int(input[j])+int(input[k])))
-                    return int(input[i])*int(input[j])*int(input[k])
+            while(k<len(data)):
+                if(data[i]+data[j]+data[k]==2020):
+                    print(f"{data[i]} + {data[j]} + {data[k]} = {data[i]+data[j]+data[k]}")
+                    return data[i]*data[j]*data[k]
                 k=k+1
             j=j+1
         i=i+1
 
 # Make sure everything look OK
-assert day1(validation1.splitlines()) == correctAnswer1
-assert day1(validation2.splitlines()) == correctAnswer1
+assert day1(validation1) == correctAnswer1
+assert day1(validation2) == correctAnswer1
 
 f = open("input.txt", "r")
 data = f.read().splitlines()
@@ -55,7 +59,10 @@ data = f.read().splitlines()
 print(day1(data))
 
 # Make sure everything look OK again
-assert day2(validation1.splitlines()) == correctAnswer2
-assert day2(validation2.splitlines()) == correctAnswer2
+assert day2(validation1) == correctAnswer2
+assert day2(validation2) == correctAnswer2
 
 print(day2(data))
+
+end = time.time()
+print(end - start)
