700c6f413717cd1e02cb1680289cc205e7a079ce
Lukas <segelmark.lukas@gmail.com>
2020-12-12 00:34:44 +0100

    Clean up code

--- 2020/day11/day11.py
+++ 2020/day11/day11.py
@@ -10,60 +10,40 @@ def renderMap(seat_map):
         s = "" 
         print(s.join(line))
 
-def count_occupied_adj(pos,seats):
-    y=pos[0]
-    x=pos[1]
-    maxY=len(seats)-1
-    maxX=len(seats[0])-1
-    occupied="#"
-    c=0
-    if y>0:
-        if x>0 and seats[y-1][x-1]==occupied: c+=1
-        if seats[y-1][x]==occupied: c+=1
-        if x<maxX and seats[y-1][x+1]==occupied: c+=1
-    if y<maxY:
-        if x>0 and seats[y+1][x-1]==occupied: c+=1
-        if seats[y+1][x]==occupied: c+=1
-        if x<maxX and seats[y+1][x+1]==occupied: c+=1
-    if x>0 and seats[y][x-1]==occupied: c+=1
-    if x<maxX and seats[y][x+1]==occupied: c+=1
-    return c
-
-def count_occupied_visible(pos,seats):
-    y0=pos[0]
-    x0=pos[1]
-    maxY=len(seats)
-    maxX=len(seats[0])
-    occupied="#"
-    free="L"
+def count_occupied_visible(pos,seats,visibility=0):
+    if not visibility:
+        visibility=len(seats)
     c=0
     directions=[[-1,-1],[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1]]
     for d in directions:
-        y=y0+0
-        x=x0+0
-        while((y>=0 and y<maxY and x>=0 and x<maxX)):
+        y=pos[0]+0
+        x=pos[1]+0
+        i=0
+        while(i<visibility):
             y+=d[0]
             x+=d[1]
-            if not (y>=0 and y<maxY and x>=0 and x<maxX):
+            if not (y>=0 and y<len(seats) and x>=0 and x<len(seats[0])):
                 break
             check=seats[y][x]
-            if check==occupied:
+            if check=="#": #occupied
                 c+=1
                 break
-            if check==free:
+            if check=="L": #free
                 break
+            i+=1
     return c
 
-print(count_occupied_visible([4,3],v8))
+assert count_occupied_visible([4,3],v8) == 8
+assert count_occupied_visible([4,3],v8,1) == 2
 
-def applyRule(seat_map):
+def applyRule(seat_map,visibility=0):
     new_map=[]
     for y, line in enumerate(seat_map):
         new_map.append(seat_map[y].copy())
         for x, char in enumerate(line):
-            if char == "L" and count_occupied_visible([y,x],seat_map)==0: # If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
+            if char == "L" and count_occupied_visible([y,x],seat_map,visibility)==0: # If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
                 new_map[y][x]="#"
-            if char == "#" and count_occupied_visible([y,x],seat_map)>=5: # If a seat is occupied (#) and four or more seats adjacent to it are also occupied, the seat becomes empty.
+            if char == "#" and count_occupied_visible([y,x],seat_map,visibility)>=5-visibility: # If a seat is occupied (#) and four or more seats adjacent to it are also occupied, the seat becomes empty.
                 new_map[y][x]="L"
     return new_map
 
@@ -73,22 +53,22 @@ def countCharacters(char, seat_map):
         c+=row.count(char)
     return c
 
-def day11(data):
+def day11(data,visibility=0):
     seat_map=[convert(line) for line in data]
     newcount=countCharacters("#",seat_map)
     oldcount=1
     while(oldcount!=newcount):
         # renderMap(seat_map)
-        seat_map=applyRule(seat_map)
+        seat_map=applyRule(seat_map,visibility)
         oldcount=newcount
         newcount=countCharacters("#",seat_map)
     return countCharacters("#",seat_map)
 
-# assert day11(validationdata)==37
-
-print(day11(validationdata))
+assert day11(validationdata,1)==37 # Run with visibility limited to 1
+assert day11(validationdata)==26 # See further!
 
 f = open("input.txt", "r")
 data = f.read().splitlines()
 
-print(day11(data))
+print(day11(data,1)) # Run with visibility limited to 1
+print(day11(data)) # See further!
