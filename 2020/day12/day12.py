9047b13a49a9553202e1bdbf9a06dfa5a76c1476
Lukas <segelmark.lukas@gmail.com>
2020-12-12 23:11:55 +0100

    Make easier to read

--- 2020/day12/day12.py
+++ 2020/day12/day12.py
@@ -1,44 +1,57 @@
 from validationdata import *
 
-def navigate(param,value,pos):
+def navigate(action,value,pos):
     """ Action N/S/E/W means to move ship or waypoint north/south/east/west by the given value. """
-    direction={"N":[1,0],"E":[0,1],"S":[-1,0],"W":[0,-1]}
-    return [pos[0]+direction[param][0]*value,pos[1]+direction[param][1]*value]
+    direction={ "N": [1,0],
+                "E": [0,1],
+                "S": [-1,0],
+                "W": [0,-1]}
+    return [pos[0]+direction[action][0]*value,
+            pos[1]+direction[action][1]*value]
 
-def rotateWaypoint(param,value,pos,wp):
-    """ Action R now means to rotate the waypoint around the ship right (clockwise) the given number of degrees. """
-    waypoint={90:[-wp[1],wp[0]],180:[-wp[0],-wp[1]],270:[wp[1],-wp[0]]}
-    if param=="L" and (value==90 or value==270): value=(value+180)%360 # turning left is like turning right backwards
-    return pos, waypoint[value]
+def rotateWaypoint(action,value,pos,wp):
+    """ Action R rotates the waypoint around the ship right (clockwise) the given number of degrees. """
+    waypoint={  90:[ -wp[1], wp[0] ],
+                180:[ -wp[0], -wp[1] ],
+                270:[ wp[1], -wp[0] ] }
+    if action=="L" and (value==90 or value==270): # turning left is like turning right backwards
+        value=(value+180)%360 
+    return waypoint[value]
 
-def moveToWaypoint(param,value,pos,waypoint):
-    """ Action F now means to move to the waypoint a number of times equal to the given value. """
-    return [pos[0]+waypoint[0]*value,pos[1]+waypoint[1]*value], waypoint
+def moveToWaypoint(action,value,pos,waypoint):
+    """ Action F moves to the waypoint a number of times equal to the given value. """
+    return [pos[0]+waypoint[0]*value,
+            pos[1]+waypoint[1]*value]
 
-def executeCommand(param,value,pos,waypoint,mode="default"):
-    if(mode=="waypoint" and param in ["N","W","E","S"]): return pos, navigate(param,value,waypoint)
-    elif(mode=="default" and param in ["N","W","E","S"]): return navigate(param,value,pos), waypoint
-    elif(param in ["L","R"]): return rotateWaypoint(param,value,pos,waypoint)
-    elif(param == "F"): return moveToWaypoint(param,value,pos,waypoint)
+def executeCommand(action,value,pos,waypoint,mode="default"):
+    """ The right function depends on the action as well as the mode of vessel navigation """
+    if mode=="waypoint" and action in ["N","W","E","S"]:
+        return pos, navigate(action,value,waypoint)
+    elif mode=="default" and action in ["N","W","E","S"]:
+        return navigate(action,value,pos), waypoint
+    elif action in ["L","R"]:
+        return pos, rotateWaypoint(action,value,pos,waypoint)
+    elif action == "F":
+        return moveToWaypoint(action,value,pos,waypoint), waypoint
 
-def runShip(instructions,mode="default",waypoint=[0,1]):
+def sailShip(instructions,mode="default",waypoint=[0,1]):
+    """ Automatically sails ships according to instructions on discrete grids """
     pos=[0,0]
-    if(mode=="waypoint"): waypoint=[1,10]
     for inst in instructions:
-        pos, waypoint = executeCommand(inst[0],int(inst[1:]),pos,waypoint,mode)
+        pos, waypoint = executeCommand(inst[0], int(inst[1:]), pos, waypoint, mode)
     return pos
 
-assert runShip(validationdata)==[-8,17]
-assert runShip(validationdata,"waypoint") == [-72, 214]
+assert sailShip(validationdata)==[-8,17]
+assert sailShip(validationdata,"waypoint",[1,10]) == [-72, 214]
 
 def manhattan(data):
     return abs(data[0])+abs(data[1])
 
-assert manhattan(runShip(validationdata)) == 25
-assert manhattan(runShip(validationdata,"waypoint")) == 286
+assert manhattan(sailShip(validationdata)) == 25
+assert manhattan(sailShip(validationdata,"waypoint",[1,10])) == 286
 
 f = open("input.txt", "r")
 data = f.read().splitlines()
 
-print(manhattan(runShip(data))) # Part 1
-print(manhattan(runShip(data,"waypoint"))) # Part 2
+print(manhattan(sailShip(data))) # Part 1
+print(manhattan(sailShip(data,"waypoint",[1,10]))) # Part 2
